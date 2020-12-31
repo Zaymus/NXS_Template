b0496f9ad4f9570e0251c94a51297d6d84700a1f
Carson Brown <45776517+Zaymus@users.noreply.github.com>
2020-12-31 03:44:17 -0500

    added angle wrapper #define

--- include/Headers/Functions.hpp
+++ include/Headers/Functions.hpp
@@ -7,6 +7,7 @@
 #define radToDeg(radians) radians/M_PI *180// converts radians to degrees
 #define log(a) cout << #a << ": " << a// custom logging define that outputs the name and value of a variable to the console
 #define sgn(x) x > 0 ? 1 : -1// returns the sign of a given number
+#define angleWrap(angle) angle > degToRad(360) ? angle - degToRad(360) : angle < 0 ? angle + degToRad(360) : angle
 
 extern void print(void*);
 #endif
